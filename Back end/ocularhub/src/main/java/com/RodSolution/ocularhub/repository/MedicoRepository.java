c85c04ecdd3a74a03176aab1dad91b5509d4c539
rodrigo <kakakpegado@hotmail.com>
2026-04-19 23:17:00 -0300

    feat: implementacao da interface MedicoRepository

---
+++ Back end/ocularhub/src/main/java/com/RodSolution/ocularhub/repository/MedicoRepository.java
@@ -0,0 +1,8 @@
+package com.RodSolution.ocularhub.repository;
+
+import com.RodSolution.ocularhub.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    
+}
